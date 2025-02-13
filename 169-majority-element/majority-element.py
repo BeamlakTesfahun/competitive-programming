3dcc7539137a832b51c09526f3d361d3f6b24d98
Beamlak Tesfahun <beamlakt24@gmail.com>
2025-02-13 20:55:35 +0300

    Time: 9 ms (35.40%) | Memory: 19.7 MB (5.10%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        times = int(len(nums) / 2)
-        
-        c = collections.Counter(nums)
-        
-        for key,value in c.items():
-            if value > times:
+        target = int(len(nums) / 2)
+        n = Counter(nums)
+        print(n)
+
+        for key,value in n.items():
+            if value > target:
                 return key
 
+        
